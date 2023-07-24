ddfc9d3b0f0687e4ae31ffb037d374f19e265d97
0110Shruti github <shruti20csu274@ncuindia.edu>
2023-07-24 15:26:37 +0530

    First commit

---
+++ services/AgentService.js
@@ -0,0 +1,170 @@
+const http = require('http');
+
+const hostname = process.env.ACME_AGENT_HOST || 'localhost';
+const port = 8021;
+
+console.log('Agent is running on: ' + `http://${hostname}:${port}`);
+
+function httpAsync(options, body) {
+    return new Promise(function (resolve, reject) {
+        const req = http.request(options, (res) => {
+            const { statusCode } = res;
+            const contentType = res.headers['content-type'];
+
+            let e;
+            if (statusCode !== 200) {
+                e = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                e = new Error('Invalid content-type.\n' + `Expected application/json but received ${contentType}`);
+            }
+            if (e) {
+                // Consume response data to free up memory
+                res.resume();
+                return reject(e);
+            }
+
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    return resolve(parsedData);
+                } catch (e) {
+                    return reject(e);
+                }
+            });
+        }).on('error', (e) => {
+            return reject(e);
+        });
+        
+        if (body) {
+            req.write(body || '');
+        }
+        
+        req.end();
+    });
+}
+
+class AgentService {
+    async getStatus() {
+        try {
+            const response = await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/status',
+                method: 'GET'
+            });
+            return response;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async getConnections() {
+        try {
+            const response = await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/connections',
+                method: 'GET'
+            });
+            return response.results;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    async createInvitation() {
+        try {
+            const response = await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/connections/create-invitation',
+                method: 'POST'
+            });
+            return response;
+        } catch (error) {
+            console.error(error);
+            return {};
+        }
+    }
+
+    async receiveInvitation(invitation) {
+        try {
+            const response = await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/connections/receive-invitation',
+                method: 'POST'
+            }, invitation);
+            return response;
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+    }
+
+    async removeConnection(connectionId) {
+        try {
+            await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: `/connections/${connectionId}/remove`,
+                method: 'POST'
+            });
+        } catch (error) {
+            console.error(error);
+        } finally {
+            return;
+        }
+    }
+
+    async getProofRequests() {
+        try {
+            const response = await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/present-proof/records',
+                method: 'GET'
+            });
+            return response.results;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    async sendProofRequest(proofRequest) {
+        try {
+            await httpAsync({
+                hostname: hostname,
+                port: port,
+                path: '/present-proof/send-request',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }, proofRequest);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            return;
+        }
+    }
+}
+
+module.exports = new AgentService();
+// The line const hostname = process.env.ACME_AGENT_HOST || 'localhost'; assigns the value of the environment variable ACME_AGENT_HOST to the hostname variable. If the ACME_AGENT_HOST environment variable is not set or is empty, it falls back to the default value 'localhost'.
+
+// The line const port = 8041; simply assigns the value 8041 to the port variable.
+
+// Together, these lines determine the hostname and port where the agent is expected to be running. If the ACME_AGENT_HOST environment variable is set, it will be used as the hostname. Otherwise, the default value 'localhost' will be used. The port is always set to 8041.
+
+
+
+
+
+
